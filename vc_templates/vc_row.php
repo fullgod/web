0e825eee795a0d652d68f60b8dd97530a1b9da3d
fullgod <fullgod@gmail.com>
2015-05-05 16:19:01 +0300

    123

--- vc_templates/vc_row.php
+++ vc_templates/vc_row.php
@@ -102,9 +102,9 @@ if( 'image-left' == $background_style ){
 	$css_class = apply_filters( VC_SHORTCODE_CUSTOM_CSS_FILTER_TAG, 'vc_row wpb_row '. ( $this->settings('base')==='vc_row_inner' ? 'vc_inner ' : '' ) . get_row_css_class() . $el_class . vc_shortcode_custom_css_class( $css, ' ' ), $this->settings['base'], $atts );
 	
 	$style = $this->buildStyle($bg_image, $bg_color, $bg_image_repeat, $font_color, $padding, $margin_bottom);
-	$output .= '<section class="'. $background_style .' '.$css_class.'"'.$style.'>';
+	$output .= '<section class="'. $background_style .' '.$css_class.'"'.$style.'><div class="container-fluid"><div class="row">';
 	$output .= wpb_js_remove_wpautop($content);
-	$output .= '</section>'.$this->endBlockComment('row');
+	$output .= '</div></div></section>'.$this->endBlockComment('row');
 
 } else {
 	
